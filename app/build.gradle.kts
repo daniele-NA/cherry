32e9750a77a7bde4455e1dc4bf131669cdf7af10
daniele-NA <crescenziaziendale@gmail.com>
2026-03-22 17:56:58 +0100

    removed proguard-rules.pro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = true
         }
     }
     compileOptions {
